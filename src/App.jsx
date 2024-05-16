3b239445d2c0d5fd2743dc5c6d4ba228ec60e4a6
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-05-16 11:29:24 +0200

    context created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { ActivitiesProvider } from "./assets/Context/ActivitiesContext";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <ActivitiesProvider>{/* MAIN ET OUTLET */}</ActivitiesProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
