773f4847794d4648ee1207389515bba0ad69b2d5
eloregnier <elodie.regnier.92@gmail.com>
2024-05-17 11:08:47 +0200

    fix images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,31 @@
 import "./footer.css";
+import bannerFooter from "../../public/images/footerbanner2.svg";
 
 function Footer() {
   return (
     <>
-    <img className="footer-banner" src="./public/images/footerbanner2.svg"/>
+      <img className="footer-banner" src={bannerFooter} />
       <div className="footer">
         <p className="devTitle">
           Protojam <br /> Developpé en 24h
         </p>
         <img
           className="logoWCS"
-          src="./public/images/LogoWildwhite.png"
+          src="../../public/images/LogoWildwhite.png"
           alt="logo Wild Code School"
         />
         <div className="devBlock">
           <p className="devTitle">Developeuses</p>
           <ul className="dev">
-            <li><span className="icon-flower">✿</span> Audrey Baudry</li> 
-            <li><span className="icon-flower">✿</span> Emilie Lingat</li> 
-            <li><span className="icon-flower">✿</span> Elodie Regnier</li>
+            <li>
+              <span className="icon-flower">✿</span> Audrey Baudry
+            </li>
+            <li>
+              <span className="icon-flower">✿</span> Emilie Lingat
+            </li>
+            <li>
+              <span className="icon-flower">✿</span> Elodie Regnier
+            </li>
           </ul>
         </div>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import "./header.css";
 import { Link } from "react-router-dom";
+import yourSVGDesktop from "../../public/images/HomePage/lilleauvert2.svg";
+import yourSVGMobile from "../../public/images/HomePage/lilleauvertmobile.svg";
 
 function Header() {
-  return (<>
-    <Link to ="/"> <img className="img-banner" src="./public/images//HomePage/lilleauvert2.svg"/> </Link>
-    <img className="img-banner-mobile" src="./public/images//HomePage/lilleauvertmobile.svg"></img>
-
+  return (
+    <>
+      <Link to="/">
+        {" "}
+        <img className="img-banner" src={yourSVGDesktop} />{" "}
+      </Link>
+      <img className="img-banner-mobile" src={yourSVGMobile}></img>
     </>
   );
 }
